e3e2f087f2bc89ed452299a08b9dedf6983c786a
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-20 11:38:40 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+    private
+  
+      def ensure_current_user_is_superadmin!
+        authenticate_user!
+  
+        unless current_user.super_admin?
+          redirect_to root_path, status: :forbidden, alert: "Unauthorized Access!"
+        end
+      end
 end
+
+  
