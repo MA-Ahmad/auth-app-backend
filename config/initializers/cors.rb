bfd2d3173ddde8ec2163e0d078e088ca02972d7b
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-20 01:47:33 -0700

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,14 +16,21 @@
 # end
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins "http://localhost:3000"
-      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    end
+    # allow do
+    #   origins "http://localhost:3000"
+    #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    # end
   
+    # allow do
+    #   origins "https://auth-app-rails-back.herokuapp.com"
+    #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    # end
     allow do
-      origins "https://auth-app-rails-back.herokuapp.com"
-      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+      origins '*'
+  
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end
   
